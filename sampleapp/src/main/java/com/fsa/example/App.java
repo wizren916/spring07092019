4596c5665d4c8b9b0c9562b17cd1de75e8705f0b
Wizard <rejiv77@gmail.com>
2019-07-13 10:14:35 -0500

    First Commit

---
+++ sampleapp/src/main/java/com/fsa/example/App.java
@@ -0,0 +1,10 @@
+package com.fsa.example;
+
+public class App {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.speak();
+	}
+
+}
---
+++ sampleapp/src/main/java/com/fsa/example/Person.java
@@ -0,0 +1,7 @@
+package com.fsa.example;
+
+public class Person {
+	public void speak() {
+		System.out.println("Hello World!");
+	}
+}
