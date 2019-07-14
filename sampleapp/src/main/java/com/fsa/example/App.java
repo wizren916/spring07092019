18e959db2a54fc9405af129584a5c6174c4801b5
Wizard <rejiv77@gmail.com>
2019-07-13 19:22:30 -0500

    ClassPathXmlApplicationContext

--- sampleapp/src/main/java/com/fsa/example/App.java
+++ sampleapp/src/main/java/com/fsa/example/App.java
@@ -1,15 +1,15 @@
 package com.fsa.example;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Person person = (Person) context.getBean("person");
 		person.speak();
-		((FileSystemXmlApplicationContext) context).close();
+		((ClassPathXmlApplicationContext) context).close();
 	}
 
 }
