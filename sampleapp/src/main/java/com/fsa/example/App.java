eb76b01eb7fd3b195deca1b6b205dd6ec981eb4f
Wizard <rejiv77@gmail.com>
2019-07-14 09:39:35 -0500

    Bean scope

--- sampleapp/src/main/java/com/fsa/example/App.java
+++ sampleapp/src/main/java/com/fsa/example/App.java
@@ -8,7 +8,16 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/fsa/example/beans.xml");
 		Person person = (Person) context.getBean("person");
+		Person person2 = (Person) context.getBean("person");
+		
 		System.out.println(person);
+		System.out.println(person2);
+		
+		person.setTaxId(9999);
+		
+		System.out.println(person);
+		System.out.println(person2);
+		
 		person.speak();
 		((ClassPathXmlApplicationContext) context).close();
 	}
