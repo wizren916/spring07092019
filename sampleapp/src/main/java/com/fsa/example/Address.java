23c6f2ea7e1021a1194068ed64d789759ee19d7e
Wizard <rejiv77@gmail.com>
2019-07-15 21:11:43 -0500

    P namespace

--- sampleapp/src/main/java/com/fsa/example/Address.java
+++ sampleapp/src/main/java/com/fsa/example/Address.java
@@ -3,9 +3,20 @@ package com.fsa.example;
 public class Address {
 	private String street;
 	private int postcode;
+	
+	public Address() {
+	}
 
 	public Address(String street, int postcode) {
 		this.street = street;
+		this.postcode = postcode;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public void setPostcode(int postcode) {
 		this.postcode = postcode;
 	}
 
--- sampleapp/src/main/java/com/fsa/example/App.java
+++ sampleapp/src/main/java/com/fsa/example/App.java
@@ -9,10 +9,12 @@ public class App {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/fsa/example/beans.xml");
 		Person person = (Person) context.getBean("person");
 		Person person2 = (Person) context.getBean("person2");
-		
+
 		System.out.println(person);
 		System.out.println(person2);
-		
+
+		Address address2 = (Address) context.getBean("address2");
+		System.out.println(address2);
 		((ClassPathXmlApplicationContext) context).close();
 	}
 
